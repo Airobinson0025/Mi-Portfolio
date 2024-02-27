40e9596c00797a5aa9f497087637816ceb2c2bc8
Aaron Robinsom <airobinson0025@gmail.com>
2024-02-27 12:35:42 -0500

    chore: css change to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -17,14 +17,14 @@ const Navbar = () => {
             }
         }
 
-        // Add event listener when component mounts
+        
         window.addEventListener('scroll', changeNavbarColor)
 
-        // Remove event listener when component unmounts
+        
         return () => {
             window.removeEventListener('scroll', changeNavbarColor)
         }
-    }, []) // Empty dependency array to ensure this effect runs only once on mount
+    }, [])
 
     return (
         <div style={{ backgroundColor: backgroundColor, boxShadow: boxShadow }} className='flex items-center justify-between fixed w-full p-3 font-dm text-neutral-50 transition duration-200'>
@@ -34,19 +34,19 @@ const Navbar = () => {
 
             <nav className='hidden md:inline'>
                 <ul className='flex items-center space-x-6 text-[1.25rem]'>
-                    <li className='hover:scale-105 hover:text-lime-300 transition duration-200'>
+                    <li className=' hover:text-lime-300 transition duration-200'>
                         <Link href='#tools'>Tools</Link>
                     </li>
-                    <li className='hover:scale-105 hover:text-lime-300 transition duration-200'>
+                    <li className=' hover:text-lime-300 transition duration-200'>
                         <Link href='#projects'>Projects</Link>
                     </li>
-                    <li className='hover:scale-105 hover:text-lime-300 transition duration-200'>
+                    <li className=' hover:text-lime-300 transition duration-200'>
                         <Link href='#samples'>Samples</Link>
                     </li>
-                    <li className='hover:scale-105 hover:text-lime-300 transition duration-200'>
+                    <li className=' hover:text-lime-300 transition duration-200'>
                         <Link href='#blog'>Blog</Link>
                     </li>
-                    <li className='hover:scale-105 hover:text-lime-300 transition duration-200'>
+                    <li className=' hover:text-lime-300 transition duration-200'>
                         <Link href='#contact'>Contact</Link>
                     </li>
                 </ul>
